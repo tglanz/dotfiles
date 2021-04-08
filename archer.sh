d086f3c64bb5eccbb23019f78436d8f8968ada2f
Tal Glanzman <talglanzman@gmail.com>
2021-04-09 01:51:28 +0300

    finalize archer

--- archer.sh
+++ archer.sh
@@ -5,23 +5,29 @@ dot_dir="$(realpath $(dirname $0))"
 cfg_dir="$XDG_CONFIG_HOME"
 [[ -z $cfg_dir ]] && cfg_dir="$HOME/.config";
 
-echo "Updating rofi"
-rofi_dir="$cfg_dir/rofi"
-mkdir -p $rofi_dir
-for file in config.rasi themes; do
-    ln -sf $dot_dir/rofi/$file $rofi_dir/$file
+function status_line() {
+    printf "$1 %-10s %s\n" "$2" "$3"
+}
+
+function symlink_as_is() {
+    mkdir -p $2
+    for file in $(ls $1); do ln -sf $1/$file $2/$file; done
+}
+
+for item in rofi xbindkeys bspwm polybar ranger alacritty ranger htop; do
+    target="$cfg_dir/$item"
+    status_line "updating" "$item" "$target" 
+    symlink_as_is $dot_dir/$item "$target"
 done
 
-exit
-
-echo "Updating vim"
 vim_dir="$HOME/.vim"
+status_line "updating" "vim" "$vim_dir"
 mkdir -p $vim_dir
 ln -sf $dot_dir/vim/vimrc $vim_dir/vimrc
 mkdir -p $vim_dir/bundle
 if [[ ! -d $vim_dir/bundle/Vundle.vim ]]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git $vim_dir/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git $vim_dir/bundle/Vundle.vim >/dev/null
 else
-    git -C $vim_dir/bundle/Vundle.vim pull
+    git -C $vim_dir/bundle/Vundle.vim pull >/dev/null
 fi
 vim +PluginInstall +qall
