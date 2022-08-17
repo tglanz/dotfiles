94a44b2c231901f3d25fd2d0114e227744a1dfc1
Tal Glanzman <talglanzman@gmail.com>
2022-08-18 02:36:22 +0300

    lualine mostly

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,6 +2,12 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use 'EdenEast/nightfox.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+      'kyazdani42/nvim-web-devicons', opt = true
+    }
+  }
 
   use {
     'phaazon/hop.nvim',
--- nvim/lua/setup-hop.lua
+++ nvim/lua/setup-hop.lua
@@ -2,3 +2,4 @@ local hop = require('hop')
 local hint = require('hop.hint')
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', 'f', ':HopWord<CR>', opts)
+vim.api.nvim_set_keymap('n', 'F', ':HopAnywhere<CR>', opts)
