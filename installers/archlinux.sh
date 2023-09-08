5837d467d5cf64b60853491d50b9ce2afc05c66b
Tal Glanzman <talglanzman@gmail.com>
2023-09-08 11:47:31 +0300

    installers and modify sound control bindings

--- installers/archlinux.sh
+++ installers/archlinux.sh
@@ -8,15 +8,18 @@ systemctl enable --now ntpd
 pacman -Suy bluez bluez-utils
 systemctl enable --now bluetooth
 
+pacman -Suy autorandr
+
 pacman -Suy alsa-utils pipewire pipewire-docs pipewire-audio wireplumber pipewire-alsa
 pacman -Suy libinput
 
+pacman -Suy ranger dolphin
+
 pacman -Suy jdk-openjdk maven
 pacman -Suy docker docker-compose
-pacman -Suy helm kubectl
+pacman -Suy kubectl helm k9s stern
 pacman -Suy go gopls delve 
 pacman -Suy terraform
-pacman -Suy autorandr
 
 pacman -Suy rg lazygit
 
