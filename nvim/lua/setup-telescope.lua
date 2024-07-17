801dca611596b10b82d6113b93bcae6009596f86
Tal Glanzman <il032650@Tals-MacBook-Pro.local>
2024-07-17 14:56:08 +0300

    ignore node_modules and .git

--- nvim/lua/setup-telescope.lua
+++ nvim/lua/setup-telescope.lua
@@ -10,6 +10,12 @@ vim.keymap.set('n', '<C-f>', builtin.live_grep, opts)
 vim.keymap.set('n', '<leader>ft', builtin.help_tags, opts)
 
 telescope.setup {
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+      ".git"
+    },
+  },
   pickers = {
     find_files = {
       hidden = true
