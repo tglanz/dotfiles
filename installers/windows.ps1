d12c3c20fe85f486fc0bede36800bbd48b3f3c82
Tal Glanzman <talglanzman@gmail.com>
2025-05-19 12:38:54 +0300

    no need for lua lang

--- installers/windows.ps1
+++ installers/windows.ps1
@@ -4,6 +4,3 @@ winget install -e --id=JesseDuffield.lazygit
 # Telescope support
 winget install sharkdp.fd
 winget install BurntSushi.ripgrep.MSVC
-
-# Some language servers
-winget install --id=LuaLS.lua-language-server -e
