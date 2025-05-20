5f2cd1bca5c68edc4b437ec349856f256606b1b6
Tal Glanzman <talglanzman@gmail.com>
2025-05-21 00:19:31 +0300

    upgrades

---
+++ distribute/archlinux-kde.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+script_dir="$(realpath $(dirname $0))"
+dot_dir="$(realpath $script_dir/..)"
+
+cfg_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+function status_line() {
+  printf "\e[32m%-10s \e[0m%-10s %s\n" "$1" "$2" "$3"
+}
+
+function symlink_as_is() {
+  status_line "Symlinking" "$1" "$2"
+  ln -sf $1 $2
+}
+
+# Symlink the binaries
+symlink_as_is $dot_dir/bin $HOME/.local
+
+# Symlink standard configurations
+for item in nvim alacritty ranger htop qutebrowser tmux; do
+  symlink_as_is $dot_dir/$item $cfg_dir
+done
+
+# vim 
+# vim_dir="$HOME/.vim"
+# status_line "updating" "vim" "$vim_dir"
+# mkdir -p $vim_dir
+# ln -sf $dot_dir/vim/vimrc $vim_dir/vimrc
+# mkdir -p $vim_dir/bundle
+# if [[ ! -d $vim_dir/bundle/Vundle.vim ]]; then
+#     git clone https://github.com/VundleVim/Vundle.vim.git $vim_dir/bundle/Vundle.vim >/dev/null
+# else
+#     git -C $vim_dir/bundle/Vundle.vim pull >/dev/null
+# fi
+# vim +PluginInstall +qall
+
+$script_dir/distribute-autorandr.sh
---
+++ installers/arch-kde.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pacman -Suy man
+
+
+pacman -Suy ranger
+
+pacman -Suy jdk-openjdk maven
+pacman -Suy docker docker-compose
+pacman -Suy kubectl helm k9s stern
+pacman -Suy go
+pacman -Suy terraform
+
+pacman -Suy meson ninja
+
+pacman -Suy rg lazygit
+
+pacman -Suy discord obsidian lazygit 
+
+pacman -Suy gimp vlc
+
