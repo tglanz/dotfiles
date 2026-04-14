6397f841b443d27a26a54218d7c32bbd91fd4089
Tal Glanzman <talglanzman@gmail.com>
2026-04-14 10:23:02 +0300

    update telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.4',
+  'nvim-telescope/telescope.nvim',
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
